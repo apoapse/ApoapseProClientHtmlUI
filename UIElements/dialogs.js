0609926b7a982933b5a0730ea0e8e2e223483a2b
Guillaume Puyal <contact@guillaume-puyal.com>
2018-11-28 17:41:52 +0100

    Added create new user form

---
+++ UIElements/dialogs.js
@@ -0,0 +1,54 @@
+$(document).on("onReady", function ()
+{
+	$(".dialog_link").click(function ()
+	{
+		var dialog = $(this).attr("data-dialog-to-open");
+		$(".dialog").fadeOut(400);
+		$("#dialog_" + dialog).fadeIn(450);
+		$("#dialog_mask").fadeIn(450);
+
+		$(document).trigger("on_opened_dialog_" + dialog);
+	});
+
+	function CloseDialog(button)
+	{
+		$(".dialog").fadeOut(400);
+		$("#dialog_mask").fadeOut(400);
+		$(".dialog form").trigger("reset"); /* TODO: might cause performances issues */
+
+		var dialog = $(button).closest(".dialog").attr("id");
+
+		$(document).trigger("on_closed_" + dialog);
+	}
+
+	$(".dialog_close_button").click(function ()
+	{
+		CloseDialog(this);
+	});
+
+	$("#dialog_mask").click(function(e) {
+		CloseDialog(this);
+	});
+
+	/*---------------------------------------------*/
+	$(document).on("validate_add_new_user", function (event, data)
+	{
+		ApoapseAPI.SendSignal("register_user", JSON.stringify(data));
+
+		$("#add_new_user_form").trigger("reset");
+	});
+
+	$(document).on("OnAddedNewUser", function (event, data)
+	{
+		data = JSON.parse(data);
+
+		$("#add_new_user_tempUsernamefield").val(data.username);
+		$("#add_new_user_temppasswordfield").val(data.temp_password);
+		$("#new_user_info_form").show();
+	});
+
+	$(document).on("on_closed_dialog_settings", function (event, data)
+	{
+		$("#new_user_info_form").hide();
+	});
+});
--- UIElements/main_interface.js
+++ UIElements/main_interface.js
@@ -9,32 +9,6 @@ currentPage = ViewEnum.room;
 /*---------------------------------------------*/
 $(document).on("onReady", function ()
 {
-	$(".dialog_link").click(function ()
-	{
-		var dialog = $(this).attr("data-dialog-to-open");
-		$(".dialog").fadeOut(400);
-		$("#dialog_" + dialog).fadeIn(450);
-		$("#dialog_mask").fadeIn(450);
-
-		$(document).trigger("on_opened_dialog_" + dialog);
-	});
-
-	function CloseDialog()
-	{
-		$(".dialog").fadeOut(400);
-		$("#dialog_mask").fadeOut(400);
-	}
-
-	$(".dialog_close_button").click(function ()
-	{
-		CloseDialog();
-	});
-
-	$("#dialog_mask").click(function(e) {
-		CloseDialog();
-	});
-
-	/*---------------------------------------------*/
 	$(document).on("UpdateUnreadMessagesCount", function (event, data)
 	{
 		data = JSON.parse(data);
@@ -69,23 +43,5 @@ $(document).on("onReady", function ()
 	});
 
 	/*---------------------------------------------*/
-	/*$(document).on("on_opened_dialog_invite_user", function ()
-	{
-		ApoapseAPI.SendSignal("request_random_password", "", function(event, data)
-		{
-			data = JSON.parse(data);
 
-			$("#registerUserPasswordField").val(data.randomPassword);
-		});
-	});
-
-	$(document).on("clear_temporary_password", function ()
-	{
-		$("#registerUserPasswordField").val("");	// We make sure the UI do not keep the temporary password
-	});*/
-
-	/*---------------------------------------------*/
-
-
-	/*---------------------------------------------*/
 });
