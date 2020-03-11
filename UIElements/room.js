1107d373a7f1f04e673f4c4e0218e1177ef240bc
Guillaume Puyal <contact@guillaume-puyal.com>
2020-03-11 15:45:06 +0100

    Added license notice

--- UIElements/room.js
+++ UIElements/room.js
@@ -1,3 +1,14 @@
+/* ----------------------------------------------------------------------------
+// Copyright (C) 2020 Apoapse
+// Copyright (C) 2020 Guillaume Puyal
+//
+// Distributed under the Apoapse Pro Client Software License. Non-commercial use only.
+// See accompanying file LICENSE.md
+//
+// For more information visit https://github.com/apoapse/
+// And https://apoapse.space/
+// ----------------------------------------------------------------------------*/
+
 function GenerateListedThread(threadData)
 {
 	var htmlContent = "";
